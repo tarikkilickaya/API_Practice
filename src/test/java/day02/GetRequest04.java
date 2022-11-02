3558fc306108d1ce239d54f16f9b9d175fe38615
tarikklckya <109520611+tarikklckya@users.noreply.github.com>
2022-11-02 23:11:17 +0300

    02.11.2022

---
+++ src/test/java/day02/GetRequest04.java
@@ -0,0 +1,34 @@
+package day02;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class GetRequest04 {
+
+    /*
+    http://dummy.restapiexample.com/api/v1/employees  url’ine
+GET request’i yolladigimda gelen response’un
+status kodunun 200 ve content type’inin “application/json”
+ve employees sayisinin 24
+ve employee’lerden birinin “Ashton Cox”
+ve gelen yaslar icinde 21, 61, ve 23 degerlerinden birinin oldugunu test edin.
+     */
+
+    @Test
+    public void get04() {
+        String url = "http://dummy.restapiexample.com/api/v1/employees";
+
+        Response response = given().when().get(url);
+        response.then().assertThat().statusCode(200).contentType(ContentType.JSON);
+
+        response.then().assertThat().body("data", hasSize(24),
+                "data.employee_name", hasItem("Ashton Cox"),
+                "data.employee_age", hasItems(21, 61, 23));
+
+    }
+}
---
+++ src/test/java/utilities/Authentication.java
@@ -0,0 +1,32 @@
+package utilities;
+
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+
+public class Authentication {
+
+    public static String generateToken() {
+        String username = "Batch81";
+        String password = "Batch81+";
+
+        Map<String, Object> postBody = new HashMap<>();
+        postBody.put("username", username);
+        postBody.put("password", password);
+        postBody.put("rememberMe", true);
+
+        String endPoint = "https://www.gmibank.com/api/authenticate";
+
+        Response response = given().contentType(ContentType.JSON).body(postBody).when().post(endPoint);
+
+        JsonPath token = response.jsonPath();
+        return token.getString("id_token");
+
+    }
+}
